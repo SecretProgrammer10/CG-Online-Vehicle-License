627f0b4a4ea9783312f3134d026cd19737241de8
ShrutiMasanta <84119993+ShrutiMasanta@users.noreply.github.com>
2021-07-23 10:53:00 +0530

    Add files via upload

--- OnlineVehicleLicense/src/main/java/com/capgemini/onlinevehiclelicense/model/Address.java
+++ OnlineVehicleLicense/src/main/java/com/capgemini/onlinevehiclelicense/model/Address.java
@@ -1,51 +1,105 @@
 package com.capgemini.onlinevehiclelicense.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+
+
+
+@Entity
 public class Address {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="address_seq")
+	@SequenceGenerator(name="address_seq",sequenceName="address_seq", allocationSize=1)
+	private int addrId;
+	private String house;
 	private String state;
 	private String city;
-	private String house;
 	private String landmark;
-	private String pincode;
+	private int pincode;
+	
+	
 	public Address() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
-	public Address(String state, String city, String house, String landmark, String pincode) {
+
+
+	public Address(int addrId, String house, String state, String city, String landmark, int pincode) {
 		super();
+		this.addrId = addrId;
+		this.house = house;
 		this.state = state;
 		this.city = city;
-		this.house = house;
 		this.landmark = landmark;
 		this.pincode = pincode;
 	}
-	public String getState() {
-		return state;
+
+
+	public int getAddrId() {
+		return addrId;
 	}
-	public void setState(String state) {
-		this.state = state;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
+
+
 	public String getHouse() {
 		return house;
 	}
+
+
 	public void setHouse(String house) {
 		this.house = house;
 	}
+
+
+	public String getState() {
+		return state;
+	}
+
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+
+	public String getCity() {
+		return city;
+	}
+
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+
 	public String getLandmark() {
 		return landmark;
 	}
+
+
 	public void setLandmark(String landmark) {
 		this.landmark = landmark;
 	}
-	public String getPincode() {
+
+
+	public int getPincode() {
 		return pincode;
 	}
-	public void setPincode(String pincode) {
+
+
+	public void setPincode(int pincode) {
 		this.pincode = pincode;
 	}
+
+
+	@Override
+	public String toString() {
+		return "Address [addrId=" + addrId + ", house=" + house + ", state=" + state + ", city=" + city + ", landmark="
+				+ landmark + ", pincode=" + pincode + "]";
+	}
+
+
+	
 }
--- OnlineVehicleLicense/src/main/java/com/capgemini/onlinevehiclelicense/model/Application.java
+++ OnlineVehicleLicense/src/main/java/com/capgemini/onlinevehiclelicense/model/Application.java
@@ -1,39 +1,97 @@
 package com.capgemini.onlinevehiclelicense.model;
 
-import java.util.Date;
+import java.time.LocalDate;
+import javax.persistence.*;
 
+
+
+@Entity
 public class Application {
-	private String applicationNumber;
-	private Date applicationDate;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="application_seq")
+	@SequenceGenerator(name="application_seq",sequenceName="application_seq", allocationSize=1)
+	private int applicationNumber;
+	@OneToOne(cascade=CascadeType.ALL,fetch = FetchType.EAGER)
+	private Applicant applicant;
+	
+	private LocalDate applicationDate=LocalDate.now();
+	@OneToOne
+	private RTOOffice rtoOffice;
+	@Enumerated(value=EnumType.STRING)
+	private ApplicationType type;
+
+	@OneToOne(cascade=CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="docid")
+	private Documents documents=null;
+	
 	private String modeOfPayment;
-	private Double amountPaid;
+	private double amountPaid;
 	private String paymentStatus;
+	
+	@OneToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	@JoinColumn(name="appointmentnumber")
+	
+	private Appointment appointment;
+	
+	@Enumerated(value=EnumType.STRING)
+	private ApplicationStatus status;
 	private String remarks;
-	public Application() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	public Application(String applicationNumber, Date applicationDate, String modeOfPayment, Double amountPaid,
-			String paymentStatus, String remarks) {
+	public Application(int applicationNumber, Applicant applicant, LocalDate applicationDate, RTOOffice rtoOffice,
+			ApplicationType type, Documents documents, String modeOfPayment, double amountPaid, String paymentStatus,
+			Appointment appointment, ApplicationStatus status, String remarks) {
 		super();
 		this.applicationNumber = applicationNumber;
-		this.applicationDate = applicationDate;
+		this.applicant = applicant;
+		this.applicationDate = LocalDate.now();
+		this.rtoOffice = rtoOffice;
+		this.type = type;
+		this.documents = documents;
 		this.modeOfPayment = modeOfPayment;
 		this.amountPaid = amountPaid;
 		this.paymentStatus = paymentStatus;
+		this.appointment = appointment;
+		this.status = status;
 		this.remarks = remarks;
 	}
-	public String getApplicationNumber() {
+	public Application() {
+		super();
+	}
+	public int getApplicationNumber() {
 		return applicationNumber;
 	}
-	public void setApplicationNumber(String applicationNumber) {
-		this.applicationNumber = applicationNumber;
+	public void setApplicationNumber(int applicationNumber) {
+		this.applicationNumber=applicationNumber;
 	}
-	public Date getApplicationDate() {
+	public Applicant getApplicant() {
+		return applicant;
+	}
+	public void setApplicant(Applicant applicant) {
+		this.applicant = applicant;
+	}
+	public LocalDate getApplicationDate() {
 		return applicationDate;
 	}
-	public void setApplicationDate(Date applicationDate) {
-		this.applicationDate = applicationDate;
+	public void setApplicationDate(LocalDate applicationDate) {
+		this.applicationDate = LocalDate.now();
+	}
+	public RTOOffice getRtoOffice() {
+		return rtoOffice;
+	}
+	public void setRtoOffice(RTOOffice rtoOffice) {
+		this.rtoOffice = rtoOffice;
+	}
+	public ApplicationType getType() {
+		return type;
+	}
+	public void setType(ApplicationType type) {
+		this.type = type;
+	}
+	public Documents getDocuments() {
+		return documents;
+	}
+	public void setDocuments(Documents documents) {
+		this.documents = documents;
 	}
 	public String getModeOfPayment() {
 		return modeOfPayment;
@@ -41,10 +99,10 @@ public class Application {
 	public void setModeOfPayment(String modeOfPayment) {
 		this.modeOfPayment = modeOfPayment;
 	}
-	public Double getAmountPaid() {
+	public double getAmountPaid() {
 		return amountPaid;
 	}
-	public void setAmountPaid(Double amountPaid) {
+	public void setAmountPaid(double amountPaid) {
 		this.amountPaid = amountPaid;
 	}
 	public String getPaymentStatus() {
@@ -53,10 +111,32 @@ public class Application {
 	public void setPaymentStatus(String paymentStatus) {
 		this.paymentStatus = paymentStatus;
 	}
+	public Appointment getAppointment() {
+		return appointment;
+	}
+	public void setAppointment(Appointment appointment) {
+		this.appointment = appointment;
+	}
+	public ApplicationStatus getStatus() {
+		return status;
+	}
+	public void setStatus(ApplicationStatus status) {
+		this.status = status;
+	}
 	public String getRemarks() {
 		return remarks;
 	}
 	public void setRemarks(String remarks) {
 		this.remarks = remarks;
 	}
+	@Override
+	public String toString() {
+		return "Application [applicationNumber=" + applicationNumber + ", applicant=" + applicant + ", applicationDate="
+				+ applicationDate + ", rtoOffice=" + rtoOffice + ", type=" + type + ", documents=" + documents
+				+ ", modeOfPayment=" + modeOfPayment + ", amountPaid=" + amountPaid + ", paymentStatus=" + paymentStatus
+				+ ", appointment=" + appointment + ", status=" + status + ", remarks=" + remarks + "]";
+	}
+	
+	
+	
 }
