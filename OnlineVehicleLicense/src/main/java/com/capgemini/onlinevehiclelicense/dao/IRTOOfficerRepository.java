65780573aa5dbaaa956a49107a1944f9d7b4a50b
driddhi <34750059+driddhi@users.noreply.github.com>
2021-07-24 10:46:15 +0530

    Add files via upload

---
+++ OnlineVehicleLicense/src/main/java/com/capgemini/onlinevehiclelicense/dao/IRTOOfficerRepository.java
@@ -0,0 +1,27 @@
+package com.capgemini.onlinevehiclelicense.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.capgemini.onlinevehiclelicense.model.Application;
+import com.capgemini.onlinevehiclelicense.model.License;
+import com.capgemini.onlinevehiclelicense.model.RTOOfficer;
+
+
+@Repository
+public interface IRTOOfficerRepository extends JpaRepository {
+	
+	public String login(RTOOfficer officer);
+	public List<Application> getAllPendingApplications();
+	public List<Application> getAllRejectedApplications();
+	public List<Application> getAllApprovedApplications();
+	public Application getApplicationById(String applicationNumber);
+	public Application updateApplicationById(String applicationNumber);
+	public License createLearnerLicense(String applicationNumber);
+	public License createDrivingLicense(String applicationNumber);
+	
+	
+	
+}
