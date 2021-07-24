c724a48d5b1672060644811b347088000e175fd3
shubham-paul-1000 <66327518+shubham-paul-1000@users.noreply.github.com>
2021-07-24 23:49:14 +0530

    Add files via upload

---
+++ OnlineVehicleLicense/src/main/java/com/capgemini/onlinevehiclelicense/exception/RecordAlreadyPresentException.java
@@ -0,0 +1,9 @@
+package com.capgemini.onlinevehiclelicense.exception;
+
+public class RecordAlreadyPresentException extends Exception{
+	
+	public RecordAlreadyPresentException(String message)
+	{
+		super(message);
+	}
+}
---
+++ OnlineVehicleLicense/src/main/java/com/capgemini/onlinevehiclelicense/exception/RecordNotFoundException.java
@@ -0,0 +1,9 @@
+package com.capgemini.onlinevehiclelicense.exception;
+
+public class RecordNotFoundException extends Exception{
+	
+	public RecordNotFoundException(String message)
+	{
+		super(message);
+	}
+}
