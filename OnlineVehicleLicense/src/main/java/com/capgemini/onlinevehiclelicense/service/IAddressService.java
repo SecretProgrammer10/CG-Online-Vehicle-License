f3f96f0f283e6dc5603491c14d638be93e37f220
ShrutiMasanta <84119993+ShrutiMasanta@users.noreply.github.com>
2021-07-28 09:53:44 +0530

    Add files via upload

--- OnlineVehicleLicense/src/main/java/com/capgemini/onlinevehiclelicense/service/IAddressService.java
+++ OnlineVehicleLicense/src/main/java/com/capgemini/onlinevehiclelicense/service/IAddressService.java
@@ -3,13 +3,8 @@ package com.capgemini.onlinevehiclelicense.service;
 import com.capgemini.onlinevehiclelicense.model.Address;
 
 public interface IAddressService {
-	public String enterState(Address addr_state);
-	public String enterCity(Address addr_city);
-	public String enterHouse(Address addr_house);
-	public String enterLandmark(Address addr_landmark);
-	public String enterPincode(Address addr_pincode);
+	public String createAddress(Address addr);
 	public String updateAddress(Address addr);
 	public String viewAddress(Address addr);
-	public String deleteAddress(Address addr);
-
+	
 }
