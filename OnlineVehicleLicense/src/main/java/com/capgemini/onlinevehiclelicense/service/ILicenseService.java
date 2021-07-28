c9ddc9dba6a315b6b48e03c82457e4d1b3b34650
SecretProgrammer10 <55909914+SecretProgrammer10@users.noreply.github.com>
2021-07-28 15:10:47 +0530

    Add files via upload

--- OnlineVehicleLicense/src/main/java/com/capgemini/onlinevehiclelicense/service/ILicenseService.java
+++ OnlineVehicleLicense/src/main/java/com/capgemini/onlinevehiclelicense/service/ILicenseService.java
@@ -2,22 +2,17 @@ package com.capgemini.onlinevehiclelicense.service;
 
 import java.util.List;
 
-import com.capgemini.onlinevehiclelicense.model.Application;
-import com.capgemini.onlinevehiclelicense.model.Appointment;
-import com.capgemini.onlinevehiclelicense.model.Documents;
+import org.springframework.http.ResponseEntity;
+
+import com.capgemini.onlinevehiclelicense.model.License;
 
 public interface ILicenseService {
 
-	public String applyForLL(Application llApplication);
-	public String applyForDL(Application dlApplication);
-	public String uploadDocuments(Documents documents);
-	public String payFees(int amount);
-	public String emailFeesReceipt(String email);
-	public String bookSlotLLTest(Appointment appointment);
-	public String bookSlotDLTest(Appointment appointment);
-	public List<Appointment> getAvailableSlots();
-	public String renewLL(Application llApplication);
-	public String renewDL(Application dlApplication);
-	public String cancelAppointment(Appointment appointment);
+	public ResponseEntity<License> addLicense(License license);
+	public ResponseEntity<License> renewLicense(License license, String licenseNumber);
+	public List<License> viewAllLicense();
+	public License viewLicenseByNumber(String licenseNumber);
+	public ResponseEntity<License> viewLicenseByType(String licenseType);
+	public ResponseEntity<License> deleteLicense(String licenseNumber);
 	
 }
