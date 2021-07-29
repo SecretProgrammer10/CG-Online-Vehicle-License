b4c53c773529edcd0ac0f13cf3793e8b68690f4c
SecretProgrammer10 <55909914+SecretProgrammer10@users.noreply.github.com>
2021-07-29 23:49:17 +0530

    Services Updated and Created

--- OnlineVehicleLicense/src/main/java/com/capgemini/onlinevehiclelicense/service/IChallanService.java
+++ OnlineVehicleLicense/src/main/java/com/capgemini/onlinevehiclelicense/service/IChallanService.java
@@ -2,14 +2,13 @@ package com.capgemini.onlinevehiclelicense.service;
 
 import java.util.List;
 
-import org.springframework.http.ResponseEntity;
 
 import com.capgemini.onlinevehiclelicense.model.Challan;
 
 public interface IChallanService {
 
 	public List<Challan> viewAllChallanDetails();
-	public ResponseEntity<Challan> getDetailsByVehicleNumber(String vehicleNumber);
+	public Challan getDetailsByVehicleNumber(String vehicleNumber);
 	public String payChallanByVehicleNumber(String vehicleNumber);
 	
 }
