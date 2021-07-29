8cffef77c0acde5d8b25ddb9a5bb8797f380ff63
SecretProgrammer10 <55909914+SecretProgrammer10@users.noreply.github.com>
2021-07-29 16:21:58 +0530

    Entity Class Changed

--- OnlineVehicleLicense/src/main/java/com/capgemini/onlinevehiclelicense/model/Appointment.java
+++ OnlineVehicleLicense/src/main/java/com/capgemini/onlinevehiclelicense/model/Appointment.java
@@ -1,40 +1,38 @@
 package com.capgemini.onlinevehiclelicense.model;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "Appointment")
 public class Appointment {
 	
-	@ManyToOne(cascade = CascadeType.ALL)
-	@JoinColumn(name = "rto_id")
 	
 	@Id
-	@Column(name = "appplication_number")
-	private String applicationNumber;
-	
 	@Column(name = "appointment_number")
 	private String appointmentNumber;
 	
-	@Column(name = "rto_id")
-	private Date rtoId;
-	
 	@Column(name = "test_date")
-	private Date testDate;
+	private LocalDate testDate;
 	
 	@Column(name = "time_slot")
-	private String timeSlot;
+	private LocalDateTime timeSlot;
 	
 	@Column(name = "test_result")
 	private String testResult;
+	
+	@OneToOne
+	private Application application;
+	
+	@ManyToOne
+	private RTOOfficer rtoOfficer;
 	
 	
     public Appointment() {
@@ -42,72 +40,154 @@ public class Appointment {
 		// TODO Auto-generated constructor stub
 	}
 
-    public Appointment(String applicationNumber, String appointmentNumber, Date rtoId, Date testDate, String timeSlot,
-			String testResult) {
+	/**
+	 * @param appointmentNumber
+	 * @param testDate
+	 * @param timeSlot
+	 * @param testResult
+	 */
+	public Appointment(String appointmentNumber, LocalDate testDate, LocalDateTime timeSlot, String testResult) {
 		super();
-		this.applicationNumber = applicationNumber;
 		this.appointmentNumber = appointmentNumber;
-		this.rtoId = rtoId;
 		this.testDate = testDate;
 		this.timeSlot = timeSlot;
 		this.testResult = testResult;
 	}
 
-	public String getApplicationNumber() {
-		return applicationNumber;
-	}
 
-	public void setApplicationNumber(String applicationNumber) {
-		this.applicationNumber = applicationNumber;
-	}
 
+	/**
+	 * @return the appointmentNumber
+	 */
 	public String getAppointmentNumber() {
 		return appointmentNumber;
 	}
 
+	/**
+	 * @param appointmentNumber the appointmentNumber to set
+	 */
 	public void setAppointmentNumber(String appointmentNumber) {
 		this.appointmentNumber = appointmentNumber;
 	}
 
-	public Date getRtoId() {
-		return rtoId;
-	}
-
-	public void setRtoId(Date rtoId) {
-		this.rtoId = rtoId;
-	}
-
-	public Date getTestDate() {
+	/**
+	 * @return the testDate
+	 */
+	public LocalDate getTestDate() {
 		return testDate;
 	}
 
-	public void setTestDate(Date testDate) {
+	/**
+	 * @param testDate the testDate to set
+	 */
+	public void setTestDate(LocalDate testDate) {
 		this.testDate = testDate;
 	}
 
-	public String getTimeSlot() {
+	/**
+	 * @return the timeSlot
+	 */
+	public LocalDateTime getTimeSlot() {
 		return timeSlot;
 	}
 
-	public void setTimeSlot(String timeSlot) {
+	/**
+	 * @param timeSlot the timeSlot to set
+	 */
+	public void setTimeSlot(LocalDateTime timeSlot) {
 		this.timeSlot = timeSlot;
 	}
 
+	/**
+	 * @return the testResult
+	 */
 	public String getTestResult() {
 		return testResult;
 	}
 
+	/**
+	 * @param testResult the testResult to set
+	 */
 	public void setTestResult(String testResult) {
 		this.testResult = testResult;
 	}
 
+	/**
+	 * @return the application
+	 */
+	public Application getApplication() {
+		return application;
+	}
+
+	/**
+	 * @param application the application to set
+	 */
+	public void setApplication(Application application) {
+		this.application = application;
+	}
+
+	/**
+	 * @return the rtoOfficer
+	 */
+	public RTOOfficer getRtoOfficer() {
+		return rtoOfficer;
+	}
+
+	/**
+	 * @param rtoOfficer the rtoOfficer to set
+	 */
+	public void setRtoOfficer(RTOOfficer rtoOfficer) {
+		this.rtoOfficer = rtoOfficer;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((appointmentNumber == null) ? 0 : appointmentNumber.hashCode());
+		result = prime * result + ((testDate == null) ? 0 : testDate.hashCode());
+		result = prime * result + ((testResult == null) ? 0 : testResult.hashCode());
+		result = prime * result + ((timeSlot == null) ? 0 : timeSlot.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Appointment other = (Appointment) obj;
+		if (appointmentNumber == null) {
+			if (other.appointmentNumber != null)
+				return false;
+		} else if (!appointmentNumber.equals(other.appointmentNumber))
+			return false;
+		if (testDate == null) {
+			if (other.testDate != null)
+				return false;
+		} else if (!testDate.equals(other.testDate))
+			return false;
+		if (testResult == null) {
+			if (other.testResult != null)
+				return false;
+		} else if (!testResult.equals(other.testResult))
+			return false;
+		if (timeSlot == null) {
+			if (other.timeSlot != null)
+				return false;
+		} else if (!timeSlot.equals(other.timeSlot))
+			return false;
+		return true;
+	}
+
 	@Override
 	public String toString() {
-		return "Appointment [applicationNumber=" + applicationNumber + ", appointmentNumber=" + appointmentNumber
-				+ ", rtoId=" + rtoId + ", testDate=" + testDate + ", timeSlot=" + timeSlot + ", testResult="
-				+ testResult + "]";
+		return "Appointment [appointmentNumber=" + appointmentNumber + ", testDate=" + testDate + ", timeSlot="
+				+ timeSlot + ", testResult=" + testResult + ", application=" + application + ", rtoOfficer="
+				+ rtoOfficer + "]";
 	}
-	
-	
-	
+    
 }
