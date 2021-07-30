a948a4fa845c695b59e370716a96032bdd5f130a
Dibyendu <dibyenduganguly.48@gmail.com>
2021-07-30 11:02:25 +0530

    gitignore updated

--- OnlineVehicleLicense/src/main/java/com/capgemini/onlinevehiclelicense/model/Challan.java
+++ OnlineVehicleLicense/src/main/java/com/capgemini/onlinevehiclelicense/model/Challan.java
@@ -54,4 +54,16 @@ public class Challan {
 	public void setAmount(Double amount) {
 		this.amount = amount;
 	}
+	/**
+	 * @return the status
+	 */
+	public String getStatus() {
+		return status;
+	}
+	/**
+	 * @param status the status to set
+	 */
+	public void setStatus(String status) {
+		this.status = status;
+	}
 }
