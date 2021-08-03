e03f2f6efdbd9a31db05a2fd28888271e2a90978
Dibyendu <dibyenduganguly.48@gmail.com>
2021-08-03 18:19:22 +0530

    global exception handler added

---
+++ OnlineVehicleLicense/src/main/java/com/capgemini/onlinevehiclelicense/exception/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.capgemini.onlinevehiclelicense.exception;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+ @ExceptionHandler(RecordNotFoundException.class)
+ public ResponseEntity<?> recordNotFoundException(RecordNotFoundException ex, WebRequest request) {
+  ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+  return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+ }
+ 
+ @ExceptionHandler(RecordAlreadyPresentException.class)
+ public ResponseEntity<?> resourceAlreadyPressnyException(RecordAlreadyPresentException ex, WebRequest request) {
+  ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+  return new ResponseEntity<>(errorDetails, HttpStatus.ALREADY_REPORTED);
+ }
+
+ @ExceptionHandler(Exception.class)
+ public ResponseEntity<?> globleExcpetionHandler(Exception ex, WebRequest request) {
+  ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+  return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+ }
+}
