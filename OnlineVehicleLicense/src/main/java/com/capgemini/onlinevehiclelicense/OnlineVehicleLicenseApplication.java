1463a10b5ff1de5742d458e6cd6175b0d26a8923
SecretProgrammer10 <raj.science100@gmail.com>
2021-08-08 23:28:16 +0530

    final Updates

--- OnlineVehicleLicense/src/main/java/com/capgemini/onlinevehiclelicense/OnlineVehicleLicenseApplication.java
+++ OnlineVehicleLicense/src/main/java/com/capgemini/onlinevehiclelicense/OnlineVehicleLicenseApplication.java
@@ -2,12 +2,14 @@ package com.capgemini.onlinevehiclelicense;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.SpringVersion;
 
 @SpringBootApplication
 public class OnlineVehicleLicenseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineVehicleLicenseApplication.class, args);
+		System.out.println(SpringVersion.getVersion());
 	}
 
 }
