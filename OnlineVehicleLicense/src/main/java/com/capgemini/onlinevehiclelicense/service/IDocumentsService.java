e59d46c80ba61ef256c66a1352b5e00274744aaf
SecretProgrammer10 <raj.science100@gmail.com>
2021-08-23 23:31:18 +0530

    View Documents Functionality Added

--- OnlineVehicleLicense/src/main/java/com/capgemini/onlinevehiclelicense/service/IDocumentsService.java
+++ OnlineVehicleLicense/src/main/java/com/capgemini/onlinevehiclelicense/service/IDocumentsService.java
@@ -1,5 +1,6 @@
 package com.capgemini.onlinevehiclelicense.service;
 
+import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -8,7 +9,9 @@ import com.capgemini.onlinevehiclelicense.model.Documents;
 public interface IDocumentsService {
 
 	public ResponseEntity<String> updateDocuments(Documents doc, String app_number);
-	public Documents viewDocuments(String app_number);
+	public ResponseEntity<Resource> viewPhoto(String applicationId);
+	public ResponseEntity<Resource> viewIdProof(String applicationId);
+	public ResponseEntity<Resource> viewAddressProof(String applicationId);
 	public ResponseEntity<String> deleteDocuments(String appl_number);
 	public ResponseEntity<String> uploadPhoto(String id, MultipartFile photo);	
 	public ResponseEntity<String> uploadIdProof(String id, MultipartFile idProof);
